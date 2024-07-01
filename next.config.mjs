931195d6b25ed11b3cba644642aecd507adf2886
kuum97 <kuum97@icloud.com>
2024-07-01 17:42:55 +0900

    Feat: mdx 확장자를 위해 next config 수정 및 테스트

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const withMDX = require("@next/mdx")();
+import nextMDX from '@next/mdx';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkGfm from 'remark-gfm';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
+    rehypePlugins: [],
+  },
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["next-mdx-remote"],
+  transpilePackages: ['next-mdx-remote'],
   // Configure `pageExtensions` to include MDX files
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
 };
 
